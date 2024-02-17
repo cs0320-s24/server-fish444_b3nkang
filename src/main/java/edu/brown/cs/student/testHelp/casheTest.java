9a74940ab6b29c878503e7a917e9204833c49c28
fish4444 <andrew_herring@brown.edu>
2024-02-17 17:41:47 -0500

    feat: dynamic proxy with only get method not working

--- src/main/java/edu/brown/cs/student/testHelp/casheTest.java
+++ src/main/java/edu/brown/cs/student/testHelp/casheTest.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class casheTest {
-  List<List<String>> listToCashe = new ArrayList<>();
+  List<List<String>> listToCashe = List.of(
+          List.of("apple", "banana", "orange"),
+          List.of("cat", "dog", "fish"),
+          List.of("red", "green", "blue"));
+
+  public List<List<String>> testMethod (List<List<String>> target) {
+
+
+    return null;
+  }
 }
