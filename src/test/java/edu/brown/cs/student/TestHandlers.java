0665abed3f0a3a0c4e878b58367049a6c79e2a0c
fish4444 <andrew_herring@brown.edu>
2024-02-21 01:50:57 -0500

    testing: add method for broadband testing

--- src/test/java/edu/brown/cs/student/TestHandlers.java
+++ src/test/java/edu/brown/cs/student/TestHandlers.java
@@ -35,91 +35,12 @@ import spark.Request;
 import spark.Response;
 import spark.Spark;
 
-/**
- * Test the actual handlers.
- *
- * <p>On Sprint 2, you'll need to deserialize API responses. Although this demo doesn't need to do
- * that, these _tests_ do.
- *
- * <p>https://junit.org/junit5/docs/current/user-guide/
- *
- * <p>Because these tests exercise more than one "unit" of code, they're not "unit tests"...
- *
- * <p>If the backend were "the system", we might call these system tests. But I prefer "integration
- * test" since, locally, we're testing how the Soup functionality connects to the handler. These
- * distinctions are sometimes fuzzy and always debatable; the important thing is that these ARE NOT
- * the usual sort of unit tests.
- *
- * <p>Note: if we were doing this for real, we might want to test encoding formats other than UTF-8
- * (StandardCharsets.UTF_8).
- */
+
+public class TestHandlers {
+
+    MockACS ACS = new MockACS();
 
 
-/**
- * this is where a data source would be nice, because we would have a method that would get the state codes
- * and broadband codes instead of just the handler, so it makes testing harder
- */
-public class BroadbandHandlerTest {
-
-    @BeforeAll
-    public static void setup_before_everything() {
-        // Set the Spark port number. This can only be done once, and has to
-        // happen before any route maps are added. Hence using @BeforeClass.
-        // Setting port 0 will cause Spark to use an arbitrary available port.
-        Spark.port(0);
-        // Don't try to remember it. Spark won't actually give Spark.port() back
-        // until route mapping has started. Just get the port number later. We're using
-        // a random _free_ port to remove the chances that something is already using a
-        // specific port on the system used for testing.
-
-        // Remove the logging spam during tests
-        //   This is surprisingly difficult. (Notes to self omitted to avoid complicating things.)
-
-        // SLF4J doesn't let us change the logging level directly (which makes sense,
-        //   given that different logging frameworks have different level labels etc.)
-        // Changing the JDK *ROOT* logger's level (not global) will block messages
-        //   (assuming using JDK, not Log4J)
-        Logger.getLogger("").setLevel(Level.WARNING); // empty name = root logger
-    }
-
-    /**
-     * Shared state for all tests. We need to be able to mutate it (adding recipes etc.) but never
-     * need to replace the reference itself. We clear this state out after every test runs.
-     */
-    //final List<Soup> menu = new ArrayList<>();
-
-    @BeforeEach
-    public void setup() {
-        // Re-initialize state, etc. for _every_ test method run
-        //this.menu.clear();
-
-        // In fact, restart the entire Spark server for every test!
-        Spark.get("loadcsv", new LoadHandler());
-        Spark.get("viewcsv", new ViewHandler());
-        Spark.get("searchcsv", new SearchHandler());
-        Spark.get("broadband", new BroadbandHandler());
-        Spark.init();
-        Spark.awaitInitialization(); // don't continue until the server is listening
-    }
-
-    @AfterEach
-    public void teardown() {
-        // Gracefully stop Spark listening on both endpoints after each test
-        Spark.unmap("/loadcsv");
-        //Spark.unmap("viewcsv");
-        Spark.unmap("/searchcsv");
-        //Spark.unmap("broadband");
-        Spark.awaitStop(); // don't proceed until the server is stopped
-    }
-
-    /**
-     * Helper to start a connection to a specific API endpoint/params
-     *
-     * @param apiCall the call string, including endpoint (NOTE: this would be better if it had more
-     *     structure!)
-     * @return the connection for the given URL, just after connecting
-     * @throws IOException if the connection fails for some reason
-     */
     private static HttpURLConnection tryRequest(String apiCall) throws IOException {
         // Configure the connection (but don't actually send the request yet)
         URL requestURL = new URL("http://localhost:" + Spark.port() + "/" + apiCall);
@@ -132,61 +53,15 @@ public class BroadbandHandlerTest {
         return clientConnection;
     }
 
-    @Test
-    // Recall that the "throws IOException" doesn't signify anything but acknowledgement to the type
-    // checker
-    public void testAPINoRecipes() throws IOException {
-        HttpURLConnection clientConnection = tryRequest("broadband");
-        // Get an OK response (the *connection* worked, the *API* provides an error response)
-        assertEquals(200, clientConnection.getResponseCode());
-
-        // Now we need to see whether we've got the expected Json response.
-        // SoupAPIUtilities handles ingredient lists, but that's not what we've got here.
-        Moshi moshi = new Moshi.Builder().build();
-        // We'll use okio's Buffer class here
-        OrderHandler.SoupNoRecipesFailureResponse response =
-                moshi
-                        .adapter(OrderHandler.SoupNoRecipesFailureResponse.class)
-                        .fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-
-
-        BroadbandHandler.
-                System.out.println(response);
-        // ^ If that succeeds, we got the expected response. Notice that this is *NOT* an exception, but
-        // a real Json reply.
-
-        clientConnection.disconnect();
-    }
-
-    @Test
-    // Recall that the "throws IOException" doesn't signify anything but acknowledgement to the type
-    // checker
-    public void testAPIOneRecipe() throws IOException {
-        menu.add(Soup.buildNoExceptions("Carrot", Arrays.asList("carrot", "onion", "celery",
-                "garlic", "ginger", "vegetable broth")));
-
-        HttpURLConnection clientConnection = tryRequest("order");
-        // Get an OK response (the *connection* worked, the *API* provides an error response)
-        assertEquals(200, clientConnection.getResponseCode());
-
-        // Now we need to see whether we've got the expected Json response.
-        // SoupAPIUtilities handles ingredient lists, but that's not what we've got here.
-        // NOTE:   (How could we reduce the code repetition?)
-        Moshi moshi = new Moshi.Builder().build();
-
-        // We'll use okio's Buffer class here
-        System.out.println(clientConnection.getInputStream());
-        OrderHandler.SoupSuccessResponse response =
-                moshi.adapter(OrderHandler.SoupSuccessResponse.class).fromJson(new
-                        Buffer().readFrom(clientConnection.getInputStream()));
-
-        Soup carrot = new Soup("Carrot", Arrays.asList("carrot", "onion", "celery",
-                "garlic" , "ginger", "vegetable broth"), false);
-        Map<String, Object> result = (Map<String, Object>) response.responseMap().get("Carrot");
-        System.out.println(result.get("ingredients"));
-        assertEquals(carrot.getIngredients(), result.get("ingredients"));
-        clientConnection.disconnect();
-    }
+    /**
+     * method to test, getting the map of codes and getting the code
+     * @param request
+     * @param response
+     * @param state
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws URISyntaxException
+     */
     @Test
     public void testAPIStateCode (Request request, Response response, String state) throws IOException, InterruptedException, URISyntaxException {
         HttpURLConnection clientConnection = tryRequest("state");
@@ -205,12 +80,9 @@ public class BroadbandHandlerTest {
         String stateParam = request.queryParams("state");
         String countyParam = request.queryParams("county");
 
-        System.out.println("State: " + stateParam);
-        System.out.println("County: " + countyParam);
 
         if (stateParam != null && countyParam != null) {
             System.out.println("is passed through");
-            // code below taken from gearup (not bothering to change var names)
             HttpRequest buildBoredApiRequest =
                     HttpRequest.newBuilder()
                             .uri(new URI("https://api.census.gov/data/2010/dec/sf1?get=NAME&for=state:*"))
@@ -231,9 +103,49 @@ public class BroadbandHandlerTest {
 
             BroadbandProxy proxyResponseLoS = new BroadbandProxy(adapter2.fromJson(sentBoredApiResponse.body()));
 
+            ACS.stateCodes = List.of(List.of("Alabama", "01"), List.of("Alaska", "02")); // keep going
+            Assert.assertEquals(ACS.stateCodes, proxyResponseLoS);
 
-            Assert.assertEquals(2, 2);
+            Assert.assertEquals(ACS.stateCodes.get(1), proxyResponseLoS.get(1));
+
+        }
+
+
+    }
+    @Test
+    public void testAPIBroadband (Request request, Response response, List<List<String>> stateCode) throws URISyntaxException, IOException, InterruptedException {
+        if (!stateCode.equals("error_bad_request")
+                && !stateCode.equals("error_not_found")) {
+            // finding the county code
+            HttpRequest buildBoredApiRequest2 =
+                    HttpRequest.newBuilder()
+                            .uri(
+                                    new URI(
+                                            "https://api.census.gov/data/2021/acs/acs1/subject/variables?get=NAME,S2802_C03_022E&for=county:*&in=state:"
+                                                    + stateCode))
+                            .GET()
+                            .build();
+            HttpResponse<String> sentBoredApiResponse2 =
+                    HttpClient.newBuilder()
+                            .build()
+                            .send(buildBoredApiRequest2, HttpResponse.BodyHandlers.ofString());
+            // build the type format, this time for county
+            Moshi moshi3 = new Moshi.Builder().build();
+            JsonAdapter<List<List<String>>> adapter3 =
+                    moshi3.adapter(
+                            Types.newParameterizedType(
+                                    List.class, Types.newParameterizedType(List.class, String.class)));
+            BroadbandProxy proxyResponseLoS2 = new BroadbandProxy(adapter3.fromJson(sentBoredApiResponse2.body()));
+
+            this.ACS.broadband = List.of(List.of("Kings County, California", "83.5", "06", "031"), List.of(
+                    "Los Angles County, California", "89.9", "06", "037"
+            )); // add more
+
+            Assert.assertEquals(this.ACS.broadband, proxyResponseLoS2);
+            Assert.assertEquals(this.ACS.broadband.get(1), proxyResponseLoS2.get(1));
 
         }
     }
 }
+
+
